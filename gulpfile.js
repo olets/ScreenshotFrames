0672e5e4b5681bf9f270f2fa3d09748813c3e664
Henry Bley-Vroman <bley.vroman@gmail.com>
2016-07-20 16:24:40 -0400

    reorg files

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ const	gulp = require('gulp'),
 		rename = require('gulp-rename');
 
 gulp.task('default', function(){
-  gulp.src(['screenshot-frames.less'])
+  gulp.src(['screenshot-frames/screenshot-frames.less'])
     .pipe(plumber({
       errorHandler: function (error) {
         console.log(error.message);
         this.emit('end');
     }}))
     .pipe(less())
-    .pipe(rename("screenshot-frames.min.css"))
+    .pipe(rename("screenshot-frames/screenshot-frames.min.css"))
     .pipe(cleancss())
     .pipe(gulp.dest("."))
 });
