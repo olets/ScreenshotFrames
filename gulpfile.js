4b0aa47f9fe357c4224469c62d37dd664d4b664d
Henry Bley-Vroman <bley.vroman@gmail.com>
2016-07-28 00:51:16 -0400

    add custom.min.css task

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,22 @@ const	gulp = require('gulp'),
 		plumber = require('gulp-plumber'),
 		rename = require('gulp-rename');
 
-gulp.task('screenshot-frames-basics', function(){
+gulp.task('screenshot-frames:custom', function(){
+  gulp.src(['./src/screenshot-frames.less'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(less())
+    .pipe(rename("screenshot-frames-custom.min.css"))
+    .pipe(cleancss({
+        keepSpecialComments: 1
+    }))
+    .pipe(gulp.dest("."))
+});
+
+gulp.task('screenshot-frames:basics', function(){
   gulp.src(['./src/screenshot-frames-basics.less'])
     .pipe(plumber({
       errorHandler: function (error) {
@@ -34,4 +49,4 @@ gulp.task('screenshot-frames', function(){
     .pipe(gulp.dest("."))
 });
 
-gulp.task('default', ['screenshot-frames','screenshot-frames-basics']);
+gulp.task('default', ['screenshot-frames','screenshot-frames:basics']);
