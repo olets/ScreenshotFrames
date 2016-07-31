fdcce3f186e762f87538e3a3fd14f00e3a3c3d17
Henry Bley-Vroman <bley.vroman@gmail.com>
2016-07-31 18:45:04 -0400

    major changes. npm package, stylesheets, gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,53 @@
+/* to build, run
+
+        gulp prepare
+
+    and then
+
+        gulp build
+*/
+
+// node
+const fs = require('fs'),
+    path = require('path');
+// npm
 const gulp = require('gulp'),
     del = require('del'),
     cleancss = require('gulp-clean-css'),
     inject = require('gulp-inject'),
     less = require('gulp-less'),
     plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    runSequence = require('run-sequence');
 
-gulp.task('inject-inline-svg', function() {
-    gulp.src('./src/**/*.less')
-        .pipe(inject(gulp.src(['./src/**/*.svg']), {
-            relative: true,
-            starttag: '<!-- inject:svg -->',
-            transform: function(filePath, file) {
-                return file.contents.toString('utf8')
-            }
-        }))
-        .pipe(gulp.dest('./temp'));
-})
-
-gulp.task('screenshot-frames:clean', function() {
-    del('./temp')
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+        .filter(function(file) {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+}
+var folders = getFolders('./src');
+gulp.task('screenshot-frames:svg', function(file) {
+    folders.map(function(folder) {
+        return gulp.src('./src/' + folder + '/*.less')
+            .pipe(inject(gulp.src('./src/' + folder + '/*.svg'), {
+                starttag: '/* inject:svg_bg_img */background-image: url(\'data:image/svg+xml;utf8,',
+                endtag: '\');/* endinject */',
+                transform: function(filePath, file) {
+                    return file.contents.toString('utf8')
+                }
+            }))
+            .pipe(gulp.dest('./temp'))
+    });
 });
 
-gulp.task('screenshot-frames:less', function() {
-    gulp.src(['./temp/screenshot-frames.less'])
+gulp.task('screenshot-frames:prep', /* ['screenshot-frames:svg'],*/ function() {
+    return gulp.src('./src/*.less')
+        .pipe(gulp.dest("./temp"))
+});
+
+gulp.task('screenshot-frames:build', ['screenshot-frames:prep'], function() {
+    return gulp.src(['./temp/screenshot-frames.less'])
         .pipe(plumber({
             errorHandler: function(error) {
                 console.log(error.message);
@@ -35,9 +59,18 @@ gulp.task('screenshot-frames:less', function() {
         .pipe(cleancss({
             keepSpecialComments: 1
         }))
-        .pipe(gulp.dest("."))
+        .pipe(gulp.dest("./"));
 });
 
-gulp.task('screenshot-frames', ['screenshot-frames:less'], 'screenshot-frames:clean');
+gulp.task('screenshot-frames:clean', function() {
+    del('./temp')
+});
 
-gulp.task('default', ['inject-inline-svg'], 'screenshot-frames');
+// gulp.task('default', function() {
+//     runSequence('screenshot-frames:build', 'screenshot-frames:clean')
+// });
+
+gulp.task('prepare', ['screenshot-frames:svg']);
+gulp.task('build', function(){
+    runSequence('screenshot-frames:build', 'screenshot-frames:clean')
+});
