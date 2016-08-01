1ff152eefef35f5d1947c8c3a430461bda2a3642
Henry Bley-Vroman <bley.vroman@gmail.com>
2016-08-01 13:30:54 -0400

    comment up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -24,21 +24,30 @@ const del = require('del'),
     rename = require('gulp-rename'),
     runSequence = require('run-sequence');
 
+// process frame stylesheets
+//
+// builds a list of a directories subdirectories
 function getFolders(dir) {
     return fs.readdirSync(dir)
         .filter(function(file) {
             return fs.statSync(path.join(dir, file)).isDirectory();
         });
 }
+// get the names of frame directories
 var folders = getFolders('./src');
-
+// get the svgs and images into the frames' stylesheets
 gulp.task('screenshot-frames:compile:frames', function(file) {
+    // start fresh
     del('./compiled/frames/*');
+    // for every frame directory
     return folders.map(function(folder) {
+        // get the child less file (will match multiple, but there should always be only one)
         return gulp.src('./src/' + folder + '/*.less')
+            // base64 encode any background images
             .pipe(base64({
                 extensions: ['jpg', 'png']
             }))
+            // inject background svg (can match multiple, but there should only ever be one)
             .pipe(inject(gulp.src('./src/' + folder + '/*.svg'), {
                 starttag: '/* inject:svg */',
                 removeTags: true,
@@ -46,49 +55,67 @@ gulp.task('screenshot-frames:compile:frames', function(file) {
                     return file.contents.toString('utf8')
                 }
             }))
+            // save
             .pipe(gulp.dest('./compiled/frames'))
     });
 });
 
+// make the custom stylesheet
+// ideally this would first run the frames' compilation, but that currently cuts off the stream
 gulp.task('screenshot-frames:compile:custom', /* ['screenshot-frames:compile:frames'],*/ function() {
+    // merge the banner, mixins, and custom stylesheet, and save
     return gulp.src(['./src/banner.css', './src/screenshot-frames-mixins.less', './src/screenshot-frames-custom.less'])
         .pipe(concat('screenshot-frames-custom.less'))
         .pipe(gulp.dest('./compiled'))
 });
 
+// make the basic and full stylesheets
+// ideally this would first run the frames' compilation, but that currently cuts off the stream
 gulp.task('screenshot-frames:compile', /* ['screenshot-frames:compile:frames'],*/ function() {
+    // merge the banner, mixins, and basic stylesheet, and save
     gulp.src(['./src/banner.css', './src/screenshot-frames-mixins.less', './src/screenshot-frames-basics.less'])
         .pipe(concat('screenshot-frames-basics.less'))
         .pipe(gulp.dest('./compiled'));
+    // merge the banner, mixins, basic stylesheet, and additional stylesheet, and save
     return gulp.src(['./src/banner.css', './src/screenshot-frames-mixins.less', './src/screenshot-frames-basics.less', './src/screenshot-frames-additional.less', './src/screenshot-frames-mixins.less'])
         .pipe(concat('screenshot-frames.less'))
         .pipe(gulp.dest('./compiled'))
 });
 
+// make the final minified stylesheets
 gulp.task('screenshot-frames:minify', function() {
+    // for each of the main stylesheets
     return gulp.src(['./compiled/*.less'])
+        // report any CSS/LESS errors
         .pipe(plumber({
             errorHandler: function(error) {
                 console.log(error.message);
                 this.emit('end');
             }
         }))
+        // compile LESS
         .pipe(less())
         .pipe(rename({ extname: ".min.css" }))
+        // minify
         .pipe(cleanCss({
             keepSpecialComments: 1
         }))
+        // save
         .pipe(gulp.dest("."))
 });
 
 gulp.task('clean:compiled', function() {
     del('./compiled')
 });
+// dev tasks
 gulp.task('clean:minified', function() {
     del(['./*.min.css'])
 })
-gulp.task('clean:all', ['clean:minified', 'clean:compiled']);
+gulp.task('clean', ['clean:minified', 'clean:compiled']);
 
+
+// main tasks
+//
 gulp.task('config', ['screenshot-frames:compile:frames']);
 // not actually a configuration task - it's the first half of the compilation task, which currently kills the stream.
 // calling it "config" is a white lie designed to 1) be memorable, 2) sound necessary (which it is)
